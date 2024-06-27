92bc0501b935146328001c6fc9cd75709f5dc814
Merlin Migan <merlin.migan@gmail.com>
2024-06-28 01:16:04 +0200

    Création du composants Navbar et maj de l'affichage du favicon !

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  
+import {
   Hero,
   PopularProducts,
   SuperQuality,
@@ -6,37 +6,40 @@ import {
   SpecialOffer,
   CustomerRewiews,
   Subscribe,
-  Footer } from "./sections";
+  Footer,
+} from "./sections";
+
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
-    <main className='relative'>
-      Nav
-    <section className='xl:padding-1 wide:padding-r padding-b'>
-      <Hero />
-    </section>
-    <section className='padding'>
-      <PopularProducts />
-    </section>
-    <section className='padding'>
-      <SuperQuality />
-    </section>
-    <section className='padding-x py-10'>
-      <Services />
-    </section>
-    <section className='padding'>
-      <SpecialOffer />
-    </section>
-    <section className='bg-pale-blue padding'>
-      <CustomerRewiews />
-    </section>
-    <section className='padding-x sm:py-32 py-16 w-full'>
-      <Subscribe />
-    </section>
-    <section className='bg-black padding-x padding-t pb-8'>
-      <Footer />
-    </section>
-  </main>
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerRewiews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
   );
 };
 
